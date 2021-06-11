bc513666f9e4663bdb3e70ba9bee3728c34d4e2f
abhishekjagushte <abhishekjagushte@gmail.com>
2021-06-11 22:07:39 +0530

    form implemented

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -10,8 +10,13 @@ import { Observable } from 'rxjs';
 })
 export class ContactMeComponent implements OnInit {
 
-  sheetID = "1sh1N0sYLL--6hHoAB5C6Cx_mKFnrtbM8labuNkkyKZk"
+  success = "success"
+  failure = "failure"
+  notTried = "not tried"
+  loading = "loading"
 
+
+  status=this.notTried
 
   contact ={
     name: "",
@@ -30,14 +35,41 @@ export class ContactMeComponent implements OnInit {
     var headers = new Headers();
     headers.append("Access-Control-Allow-Origin", "*")
 
-    this.http.post("https://script.google.com/macros/s/AKfycby-AmBgD2FIeG2nzjAgiSxB6rqRO3OGLjrOBLzONghHR4noRrqNirCsRK39YRFkA_g/exec",
-      this.contact, {
-        headers: new HttpHeaders({
-          "Access-Control-Allow-Origin": "*"  
-        })
-      }).subscribe((response) => {
-        console.log("Sent")
-      })
+    this.status=this.loading
 
+    this.http.post("https://script.google.com/macros/s/AKfycby-AmBgD2FIeG2nzjAgiSxB6rqRO3OGLjrOBLzONghHR4noRrqNirCsRK39YRFkA_g/exec",
+    JSON.stringify(this.contact),{
+        headers: {
+          "Content-Type": "text/plain"
+        }
+    } 
+    ).subscribe((response: any) => {
+        if(response.result=="success")        
+          this.status=this.success
+        else
+          this.status=this.failure
+      })
   }
 }
+
+
+
+/*       console.log(JSON.stringify(this.contact))
+        console.log( `name=${this.contact.name}&surname=${this.contact.surname}&email=${this.contact.email}&message=${this.contact.message}`)
+        console.log(response)*/ 
+// {
+//   headers: new HttpHeaders({
+//     "Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE, PUT",
+//     "Access-Control-Allow-Credentials" : "Origin, X-Requested-With, Content-Type, Accept"
+//   })
+// }
+
+/*
+{
+    "name" : "Abhishek",
+    "surname" : "Jagushte"
+    "email" : "abhishekjagushte@gmail.com"
+    "message" : "Hey there Abhishek"
+}
+*/
